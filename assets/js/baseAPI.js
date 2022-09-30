e6dc1ae338ea3f3d6073de2a81d0947e65d2ef05
Nov <15291597095@163.com>
2022-09-30 16:46:49 +0800

    完成主页面的功能与头像

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -14,6 +14,20 @@ $.ajaxPrefilter(function (config) {
   // 定义请求头
   config.contentType = 'application/json'
   // 定义请求参数
-  config.data = format2Json(config.data)
+  config.data = config.data && format2Json(config.data)
+// 统一设置请求头（有条件的加）
+  if(config.url.includes('/my')) {
+    config.headers = {
+      Authorization:localStorage.getItem('token') || ''
+    }
+  }
+  
+  config.error = function(err){
+    if(err.responseJSON?.code === 1 && err.responseJSON?.message === "身份认证失败！") {
+      localStorage.removeItem('token')
+      location.href = '/login.html'
+    }
+  }
+
 })
 
---
+++ assets/js/home.js
@@ -0,0 +1,41 @@
+const layer = layui.layer
+$(function () {
+  gitUser()
+
+})
+const gitUser = () => {
+  $.ajax({
+    method: 'GET',
+    url: '/my/userinfo',
+    success(res) {
+      console.log(res)
+      if (res.code !== 0) layer.msg(res.message)
+      gitAvatar(res.data)
+    }
+  })
+}
+
+const gitAvatar = (data) => {
+  const uuname = data.nickname || data.username
+
+  if (data.user_pic) {
+    $('.avatar').hide()
+    $('.layui-nav-img').css(src,data.user_pic).show()
+  } else {
+    $('.layui-nav-img').hide()
+    const char = data.username.charAt(0).toUpperCase()
+    $('.avatar').html(char).show()
+  }
+  $('.welcome').html(`欢迎&nbsp;&nbsp;${uuname}`)
+}
+
+$('.butLogOut').on('click',function(){
+  layer.confirm('确认退出吗?', {icon: 3, title:'提示'}, function(index){
+    //do something
+    localStorage.removeItem('token')
+    location.href = '/login.html'
+    layer.close(index);
+  })
+})
+
+
